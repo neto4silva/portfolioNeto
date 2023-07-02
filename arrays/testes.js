7fafbe38b7a6121a95c6d5910dc5d47edfc87aff
Neto <neto4silva@outlook.com>
2023-07-02 17:38:10 -0300

    Manipulação de arrays.

--- arrays/testes.js
+++ arrays/testes.js
@@ -4,13 +4,20 @@
 // console.log(arrayVazia)
 // console.log(arrayVazia.length)
 
-jantarDeHoje = ['🍔', '🌭', '🍕']
+// jantarDeHoje = ['🍔', '🌭', '🍕']
 
-jantarDeHoje.pop()
-jantarDeHoje.pop()
+// jantarDeHoje.pop()
+// jantarDeHoje.pop()
 
-jantarDeHoje.push('🍳')
-jantarDeHoje.push('🥗')
-jantarDeHoje.push('🍏')
+// jantarDeHoje.push('🍳')
+// jantarDeHoje.push('🥗')
+// jantarDeHoje.push('🍏')
 
-console.log(jantarDeHoje)
+// console.log(jantarDeHoje)
+
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
